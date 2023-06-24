d0117bf979ec4430caf10149f7ada5ceb4f48a9d
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-24 11:19:58 +0700

    Create setup_ubuntu.sh

---
+++ setup_ubuntu.sh
@@ -0,0 +1,66 @@
+# update
+sudo apt update && sudo apt upgrade -y
+[ -f /var/run/reboot-required ] && sudo reboot -f
+sudo apt install htop -y
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install screen -y
+sudo apt install wget -y
+# tạo ram ảo
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+#tắt filewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo systemctl mask --now firewalld
+# setup caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update && sudo apt install caddy -y
+mkdir -p /data/www/default
+mkdir -p /var/log/caddy/
+mkdir -p /etc/caddy/conf.d/
+chown -R caddy.caddy /data/www/default
+chown -R caddy.caddy /var/log/caddy/
+wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/Caddyfile -O /etc/caddy/Caddyfile
+# setup mariadb 10.11
+sudo apt update
+sudo apt install mariadb-server -y
+sudo mysql_secure_installation
+systemctl start mariadb
+db_pass="Thisisdbrootpassword"
+mysql -e "grant all privileges on *.* to root@'127.0.0.1' identified by \"${db_pass}\" with grant option;"
+mysql -e "grant all privileges on *.* to root@'localhost' identified by \"${db_pass}\" with grant option;"
+mysql -uroot -p${db_pass} 2>/dev/null <<EOF
+drop database if exists test;
+delete from mysql.db where user='';
+delete from mysql.db where user='PUBLIC';
+delete from mysql.user where user='';
+delete from mysql.user where user='mysql';
+delete from mysql.user where user='PUBLIC';
+flush privileges;
+exit
+EOF
+systemctl stop mariadb
+# setup php 8.2
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update
+sudo apt install php8.2 -y
+sudo apt-get install -y php8.2-cli php8.2-common php8.2-fpm php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
+# Optimization
+wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/php.ini -O /etc/php.ini
+wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/www.conf -O /etc/php-fpm.d/www.conf
+wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/my.cnf -O /etc/my.cnf
+# start
+systemctl enable mariadb
+systemctl enable php-fpm
+systemctl enable caddy
+systemctl start mariadb
+systemctl start php-fpm
+systemctl start caddy
