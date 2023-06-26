3274e33d9b4e52bc49eb48d3322cf86dd67f0d0e
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-26 10:09:47 +0700

    Update setup_ubuntu22.04.sh

--- setup_ubuntu22.04.sh
+++ setup_ubuntu22.04.sh
@@ -59,7 +59,7 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt install php8.2 -y
 sudo apt install php8.2-fpm -y
 sudo apt install php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl -y
-
+sudo php-fpm8.2 -t
 # Optimization
 wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/ubuntu/php.ini -O /etc/php/8.2/fpm/php.ini
 wget https://raw.githubusercontent.com/bibicadotnet/LCMP/main/ubuntu/www.conf -O /etc/php/8.2/fpm/pool.d/www.conf
