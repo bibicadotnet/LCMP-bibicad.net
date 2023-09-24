fb7a7224b3356e1ef9d35c49618d42cf473609cb
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-24 13:04:50 +0700

    Update php74.sh

--- php74.sh
+++ php74.sh
@@ -139,7 +139,12 @@ echo "*/1 * * * * curl https://bibica.net/wp-cron.php?doing_wp_cron > /dev/null 
 crontab simply-static
 
 # Monitor and restart PHP, Mysql, Caddy
-
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP/main/monitor/monitor_service_restart.sh -O /usr/local/bin/monitor_service_restart.sh
+chmod +x /usr/local/bin/monitor_service_restart.sh
+nohup /usr/local/bin/monitor_service_restart.sh >> ./out 2>&1 <&- &
+crontab -l > monitor_service_restart
+echo "@reboot nohup /usr/local/bin/monitor_service_restart.sh >> ./out 2>&1 <&- &" >> monitor_service_restart
+crontab monitor_service_restart
 
 
 
