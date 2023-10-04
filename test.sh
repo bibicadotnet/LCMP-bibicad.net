b7e11054bd0ec833b6e1ad2b6236dacbb313bc1d
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-10-04 23:09:52 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update 
-sudo apt update -y
+sudo apt update & sudo apt upgrade -y 
 sudo apt install htop -y
 sudo apt install zip -y
 sudo apt install unzip -y
@@ -37,19 +37,18 @@ sudo systemctl disable firewalld
 sudo systemctl mask --now firewalld
 
 # setup caddy
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update && sudo apt install caddy -y
 caddy add-package github.com/caddyserver/cache-handler
 caddy add-package github.com/caddyserver/replace-response
 caddy add-package github.com/sillygod/cdp-cache
-
 mkdir -p /data/www/default
 mkdir -p /var/log/caddy/
 mkdir -p /etc/caddy/conf.d/
-chown -R caddy.caddy /data/www/default
-chown -R caddy.caddy /var/log/caddy/
+chown -R caddy:caddy /data/www/default
+chown -R caddy:caddy /var/log/caddy/
 wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/Caddy/Caddyfile -O /etc/caddy/Caddyfile
 
 # Setup mariadb 10.11
@@ -73,9 +72,10 @@ EOF
 systemctl stop mariadb
 
 # Setup php 7.4
-sudo apt install -y lsb-release gnupg2 ca-certificates apt-transport-https software-properties-common
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install php7.4 -y
+sudo apt install -y apt-transport-https lsb-release ca-certificates wget 
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list 
+sudo apt update & sudo apt install php7.4 -y
 sudo apt install php7.4-fpm -y
 sudo apt-get install -y php7.4-cli php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-opcache -y
 sudo php-fpm7.4 -t
@@ -85,9 +85,9 @@ systemctl stop caddy
 systemctl stop mariadb
 systemctl stop php7.4-fpm
 
-wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/PHP/php.ini -O /etc/php.ini
-wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/PHP/www.conf -O /etc/php-fpm.d/www.conf
-wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/MySQL/my.cnf -O /etc/my.cnf
+#wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/PHP/php.ini -O /etc/php/7.4/fpm/php.ini
+#wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/PHP/www.conf -O /etc/php/7.4/fpm/pool.d/www.conf
+#wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/LCMP-bibicad.net/main/Debian%20GNU/my.cnf -O /etc/mysql/my.cnf
 
 # Create symbolic link
 ln -s /var/www /root/
