a3e7cf98070cb4739fba655638d42675218a50f6
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-10-04 23:20:24 +0700

    Update debian12.sh

--- Debian GNU/debian12.sh
+++ Debian GNU/debian12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update 
-sudo apt update & sudo apt upgrade -y 
+sudo apt update -y
 sudo apt install htop -y
 sudo apt install zip -y
 sudo apt install unzip -y
@@ -75,7 +75,7 @@ systemctl stop mariadb
 sudo apt install -y apt-transport-https lsb-release ca-certificates wget 
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list 
-sudo apt update & sudo apt install php7.4 -y
+sudo apt update && sudo apt install php7.4 -y
 sudo apt install php7.4-fpm -y
 sudo apt-get install -y php7.4-cli php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-opcache -y
 sudo php-fpm7.4 -t
