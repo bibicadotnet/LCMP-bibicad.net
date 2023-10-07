57a5a44411814dae428810f55c249d530b13e796
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-10-08 04:55:44 +0700

    Update debian12.sh

--- Debian GNU/debian12.sh
+++ Debian GNU/debian12.sh
@@ -41,8 +41,6 @@ sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update && sudo apt install caddy -y
-caddy add-package github.com/caddyserver/cache-handler
-caddy add-package github.com/caddyserver/replace-response
 caddy add-package github.com/sillygod/cdp-cache
 mkdir -p /data/www/default
 mkdir -p /var/log/caddy/
