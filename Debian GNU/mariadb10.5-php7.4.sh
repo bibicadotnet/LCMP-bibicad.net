523159dbab15f58deb0f1b9e178d11b6a5cf2f85
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-10-09 23:06:18 +0700

    Update mariadb10.5-php7.4.sh

--- Debian GNU/mariadb10.5-php7.4.sh
+++ Debian GNU/mariadb10.5-php7.4.sh
@@ -26,23 +26,6 @@ sudo apt install wget -y
 sudo apt install curl -y
 sudo apt install gpg -y
 
-# Set time Viet Nam
-timedatectl set-timezone Asia/Ho_Chi_Minh
-
-# setup swapfile
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-cat <<EOF > /etc/sysctl.d/99-xs-swappiness.conf
-vm.swappiness=10
-EOF
-
-# Set time Viet Nam
-timedatectl set-timezone Asia/Ho_Chi_Minh
-
 # setup swapfile
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
